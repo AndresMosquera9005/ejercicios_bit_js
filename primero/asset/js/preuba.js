32d2d4ae48de975823a5bc49350e7280c45623a0
AndresMosquera9005 <andrubjj@gmail.com>
2024-04-29 11:43:20 -0500

    punto 4

--- primero/asset/js/preuba.js
+++ primero/asset/js/preuba.js
@@ -1,4 +1,19 @@
-function porciento_cinco() {
-    resultado_descuento_1 = (((valor_matricula* descuento_5)/100))*valor_matricula;
-    console.log(resultado_descuento_1);
-}
+const btn_haga_su_memu = document.getElementById("Valor_a_pagar");
+
+
+let arr_menu =[ 
+    {nombre: "hamburgesa", precio: 10000 },
+    {nombre: "perro", precio: 10000 },
+    {nombre: "pizza", precio: 10000 },
+    {nombre: "arroz con huevo", precio: 8500 },
+    {nombre: "gaseosa colombiana", precio: 2500 },
+    {nombre: "agua brisa", precio: 2500 },
+    {nombre: "jugo hit", precio: 2500 },
+    {nombre: "jugo del valle", precio: 2500 },
+    {nombre: "limonada", precio: 2500 },
+]
+
+btn_haga_su_memu.addEventListener("click", function(){
+    const agregando = document.getElementById("menu").value;
+
+})
